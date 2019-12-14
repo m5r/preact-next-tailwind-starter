4267eec7f666dc4da53a950844b94aa5251d0144
m5r <mokht@rmi.al>
2019-12-14 20:07:58 +0100

    replace fetch with axios and swr

--- src/pages/starred/[username].tsx
+++ src/pages/starred/[username].tsx
@@ -1,36 +1,52 @@
-import { h } from "preact";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import axios from "axios";
 
-type Props = {
-    projects: { name: string; id: number; }[];
-}
+import useRequest from "../../useRequest";
+import { GithubProject } from "../../github-project";
 
-const UserStarred: NextPage<Props> = ({ projects }) => {
+type APIResponse = GithubProject[];
+
+type InitialProps = {
+    projects: GithubProject[];
+};
+
+type Props = InitialProps;
+
+const UserStarred: NextPage<Props, InitialProps> = (props) => {
     const router = useRouter();
     const { username } = router.query;
+
+    const { data } = useRequest<APIResponse>(
+		{ url: `https://api.github.com/users/${username}/starred?per_page=100` },
+		{ initialData: props.projects },
+    );
+
+    if (!data) {
+        return (
+            <div>
+                {username} has no starred projects
+            </div>
+        );
+    }
 
     return (
         <div>
             <h1>{username}'s Starred Projects</h1>
 
             <ul>
-                {projects.map(project => <li key={project.id}>{project.name}</li>)}
+                {data.map(project => <li key={project.id}>{project.name}</li>)}
             </ul>
         </div>
     );
 };
 
 UserStarred.getInitialProps = async ({ query }) => {
-    if (typeof fetch === "undefined") {
-        await import("isomorphic-fetch");
-    }
-
     const username = query.username;
     const url = `https://api.github.com/users/${username}/starred?per_page=100`;
-    const response = await fetch(url);
+    const { data } = await axios.get<APIResponse>(url);
 
-    return { projects: await response.json() };
+    return { projects: data };
 };
 
 export default UserStarred;
---
+++ src/useRequest.ts
@@ -0,0 +1,40 @@
+import useSWR, { ConfigInterface, responseInterface } from "swr";
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
+
+interface Return<Data, Error> extends Pick<responseInterface<AxiosResponse<Data>, AxiosError<Error>>, "isValidating" | "revalidate" | "error"> {
+	data: Data | undefined;
+	response: AxiosResponse<Data> | undefined;
+}
+
+export interface Config<Data = unknown, Error = unknown> extends Omit<ConfigInterface<AxiosResponse<Data>, AxiosError<Error>>, "initialData"> {
+	initialData?: Data;
+}
+
+export default function useRequest<Data = unknown, Error = unknown>(
+	request: AxiosRequestConfig,
+	{ initialData, ...config }: Config<Data, Error> = {},
+): Return<Data, Error> {
+	const { data: response, error, isValidating, revalidate } =
+		useSWR<AxiosResponse<Data>, AxiosError<Error>>(
+			request && JSON.stringify(request),
+			() => axios(request || {}),
+			{
+				...config,
+				initialData: initialData && {
+					status: 200,
+					statusText: "InitialData",
+					config: request,
+					headers: {},
+					data: initialData,
+				},
+			},
+		);
+
+	return {
+		data: response && response.data,
+		response,
+		error,
+		isValidating,
+		revalidate,
+	};
+}
