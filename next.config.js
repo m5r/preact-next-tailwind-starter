9873e75b4371ccfad42262b0f1899a22600bcc0c
m5r <mokht@rmi.al>
2019-12-14 19:44:52 +0100

    remove hack minimize css loader

--- next.config.js
+++ next.config.js
@@ -2,22 +2,6 @@ const withCSS = require("@zeit/next-css");
 const BundleAnalyzerPlugin = require("@bundle-analyzer/webpack-plugin");
 require("dotenv").config();
 
-// from https://github.com/zeit/next-plugins/issues/541#issuecomment-534863391
-function HACK_removeMinimizeOptionFromCssLoaders(config) {
-	console.warn(
-		"HACK: Removing `minimize` option from `css-loader` entries in Webpack config",
-	);
-	config.module.rules.forEach(rule => {
-		if (Array.isArray(rule.use)) {
-			rule.use.forEach(u => {
-				if (u.loader === "css-loader" && u.options) {
-					delete u.options.minimize;
-				}
-			});
-		}
-	});
-}
-
 function withPreact(nextConfig = {}) {
 	return Object.assign({}, nextConfig, {
 		webpack(config, options) {
@@ -49,18 +33,15 @@ function withPreact(nextConfig = {}) {
 
 module.exports = withCSS(withPreact({
 	webpack(config) {
-		HACK_removeMinimizeOptionFromCssLoaders(config);
-
-		const plugins = config.plugins;
 		if (process.env.BUNDLE_ANALYZER_TOKEN) {
-			plugins.push(
+			config.plugins.push(
 				new BundleAnalyzerPlugin({ token: process.env.BUNDLE_ANALYZER_TOKEN }),
 			);
 		}
 
 		return {
 			...config,
-			plugins,
+			plugins: config.plugins,
 		};
 	},
 }));
