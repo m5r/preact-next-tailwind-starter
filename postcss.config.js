87cf999ff39b2b1320680332faad9bf1d68d52b2
m5r <mokht@rmi.al>
2020-01-09 10:56:58 +0100

    upgrade dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,15 @@
 module.exports = {
 	plugins: [
+		require("postcss-easy-import"),
 		require("tailwindcss"),
-		...(process.env.NODE_ENV === `production`
-			? [
-				require("@fullhuman/postcss-purgecss")({
-					content: ["./pages/**/*.js", "./components/**/*.js"],
-					defaultExtractor: content =>
-						content.match(/[A-Za-z0-9-_:/]+/g) || []
-				}),
-				require("autoprefixer"),
-				require("cssnano")
-			]
-			: [])
-	]
+		require("@fullhuman/postcss-purgecss")({
+			content: [
+				"./src/pages/**/*.{js,jsx,ts,tsx}",
+				"./src/components/**/*.{js,jsx,ts,tsx}",
+			],
+			defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+		}),
+		require("autoprefixer"),
+		require("cssnano"),
+	],
 };
