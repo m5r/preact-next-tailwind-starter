dee3b30be88dd12e642117d5f8a23335cbeb7895
m5r <mokht@rmi.al>
2020-02-04 00:14:28 +0100

    purge css only in production

--- postcss.config.js
+++ postcss.config.js
@@ -2,16 +2,18 @@ module.exports = {
 	plugins: [
 		"postcss-easy-import",
 		"tailwindcss",
-		[
-			"@fullhuman/postcss-purgecss",
-			{
-				content: [
-					"./src/pages/**/*.{js,jsx,ts,tsx}",
-					"./src/components/**/*.{js,jsx,ts,tsx}",
-				],
-				defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-			},
-		],
 		"postcss-preset-env",
+		process.env.NODE_ENV === "production" ?
+			[
+				"@fullhuman/postcss-purgecss",
+				{
+					content: [
+						"./src/pages/**/*.{js,jsx,ts,tsx}",
+						"./src/components/**/*.{js,jsx,ts,tsx}",
+					],
+					defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+				},
+			] :
+			undefined,
 	],
 };
