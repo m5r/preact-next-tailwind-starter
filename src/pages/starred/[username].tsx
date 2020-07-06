16fd0a883310b88916595c36bce4ab91c78f459b
m5r <mokht@rmi.al>
2020-07-06 22:20:45 +0200

    ditch getInitialProps in favor of getServerSideProps

--- src/pages/starred/[username].tsx
+++ src/pages/starred/[username].tsx
@@ -1,4 +1,4 @@
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import { useRouter } from "next/router";
 import axios from "axios";
 
@@ -8,46 +8,50 @@ import { GithubProject } from "../../github-project";
 
 type APIResponse = GithubProject[];
 
-type InitialProps = {
-    projects: GithubProject[];
+type Props = {
+	projects: GithubProject[];
 };
 
-type Props = InitialProps;
+type Query = {
+	username: string;
+}
 
-const UserStarred: NextPage<Props, InitialProps> = (props) => {
-    const router = useRouter();
-    const { username } = router.query;
+const UserStarred: NextPage<Props> = (props) => {
+	const router = useRouter();
+	const username = router.query.username;
 
-    const { data: projects } = useRequest<APIResponse>(
+	const { data: projects } = useRequest<APIResponse>(
 		{ url: `https://api.github.com/users/${username}/starred?per_page=100` },
 		{ initialData: props.projects },
-    );
+	);
 
-    if (!projects) {
-        return (
-            <div>
-                {username} has no starred projects
-            </div>
-        );
-    }
+	if (!projects) {
+		return (
+			<div>
+				{username} has no starred projects
+			</div>
+		);
+	}
 
-    return (
-        <div>
-            <h1>{username}'s Starred Projects</h1>
+	return (
+		<div>
+			<h1>{username}'s Starred Projects</h1>
 
-            <ul>
-                {projects.map(project => <li key={project.id}>{project.name}</li>)}
-            </ul>
-        </div>
-    );
+			<ul>
+				{projects.map(project => <li key={project.id}>{project.name}</li>)}
+			</ul>
+		</div>
+	);
 };
 
-UserStarred.getInitialProps = async ({ query }) => {
-    const username = query.username;
-    const url = `https://api.github.com/users/${username}/starred?per_page=100`;
-    const { data } = await axios.get<APIResponse>(url);
+export const getServerSideProps: GetServerSideProps<Props, Query> = async (context) => {
+	const username = context.params?.username;
+	const url = `https://api.github.com/users/${username}/starred?per_page=100`;
+	const { data } = await axios.get<APIResponse>(url);
 
-    return { projects: data };
-};
+	return {
+		props: { projects: data },
+	}
+}
 
 export default UserStarred;
