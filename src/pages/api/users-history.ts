31ef51b5a88636fa0c0acf5c5495fcae9484c423
m5r <mokht@rmi.al>
2020-07-11 12:10:23 +0200

    add /api/users-history route

---
+++ src/pages/api/users-history.ts
@@ -0,0 +1,66 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+	users: string[];
+}
+
+type ApiError = {
+	errorMessage: string;
+}
+
+type Response = Data | ApiError;
+
+let users: string[] = [];
+
+export default async function usersHistory(req: NextApiRequest, res: NextApiResponse<Response>): Promise<void> {
+	switch (req.method) {
+		case "PUT":
+			return insertUser(req, res);
+		case "GET":
+			return getUsersHistory(res);
+		default:
+			const error = {
+				errorMessage: `Method ${req.method} Not Allowed for /api/users-history`,
+			};
+			console.error(error);
+
+			res.setHeader("Allow", ["PUT", "GET"]);
+			res.status(405).send(error);
+			return;
+	}
+}
+
+function getUsersHistory(res: NextApiResponse<Response>): void {
+	res.status(200).json({ users });
+}
+
+type Body = {
+	user: string;
+};
+
+function isValidBody(body: any): body is Body {
+	return body.hasOwnProperty("user") && body.user === null || typeof body.user === "string";
+}
+
+function insertUser(req: NextApiRequest, res: NextApiResponse<Response>): void {
+	const body = req.body;
+
+	if (!isValidBody(body)) {
+		const error: ApiError = {
+			errorMessage: "Malformed body",
+		};
+		console.error(error);
+
+		res.status(500).send(error);
+		return;
+	}
+
+	try {
+		users.push(body.user);
+	} catch (e) {
+		console.error(e);
+		return res.status(500).end();
+	}
+
+	res.status(200).end();
+}
