01fbeefdc0a75e8d3fc778c818d9f5ae90254176
m5r <mokht@rmi.al>
2020-07-15 00:09:44 +0200

    demo index

--- src/pages/demo/index.tsx
+++ src/pages/demo/index.tsx
@@ -22,19 +22,42 @@ const Index: NextPage = () => {
 
 	return (
 		<Layout>
-			<h1>Github User's Starred Projects</h1>
+			<div className="flex-1 min-w-0 pb-8">
+				<h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">
+					Github User's Starred Projects
+				</h2>
+			</div>
 
-			<form onSubmit={onFormSubmit}>
-				<label htmlFor="username">Username:</label>
-				<input
-					id="username"
-					type="text"
-					placeholder="m5r"
-					value={username}
-					onChange={e => setUsername(e.target.value)}
-				/>
+			<form
+				onSubmit={onFormSubmit}
+				className="md:flex md:items-center md:justify-between"
+			>
+				<div className="flex-1 min-w-0">
+					<label htmlFor="username" className="block text-sm font-medium leading-5 text-gray-700">GitHub Username</label>
+					<div className="mt-1 relative rounded-md shadow-sm">
+						<div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+							<span className="text-gray-500 sm:text-sm sm:leading-5">http://github.com/</span>
+						</div>
+						<input
+							id="username"
+							className="form-input block w-full pl-38 sm:pl-33 sm:text-sm sm:leading-5"
+							type="text"
+							placeholder="m5r"
+							value={username}
+							onChange={e => setUsername(e.target.value)}
+						/>
+					</div>
+				</div>
 
-				<input type="submit" value="Submit" />
+				<div className="mt-4 flex md:mt-auto md:ml-4">
+					<span className="shadow-sm rounded-md ml-auto">
+						<input
+							type="submit"
+							className="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700 transition duration-150 ease-in-out"
+							value="Submit"
+						/>
+					</span>
+				</div>
 			</form>
 
 			{Boolean(usersHistory?.users.length) && (
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
 			fontFamily: {
 				sans: ["Inter var", ...defaultTheme.fontFamily.sans],
 			},
+			spacing: {
+				33: "8.25rem",
+				34: "8.5rem",
+				38: "9.5rem",
+			},
 		},
 	},
 	variants: {},
